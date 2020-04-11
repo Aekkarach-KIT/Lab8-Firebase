aaa748e4f8dc1b0fc93aa7715a9b7596e6428648
Aekkarach-KIT <s6035512060@phuket.psu.ac.th>
2020-04-11 14:16:45 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyAAoH7iL86LIPQFnAgjFskjyT2mBzIJAss",
+    authDomain: "test-4d73b.firebaseapp.com",
+    databaseURL: "https://test-4d73b.firebaseio.com",
+    projectId: "test-4d73b",
+    storageBucket: "test-4d73b.appspot.com",
+    messagingSenderId: "353794688903",
+    appId: "1:353794688903:web:17dfb65fae7aaeb1d8b522",
+    measurementId: "G-N42M9NK1S7"
+  };
+
+  export default config
